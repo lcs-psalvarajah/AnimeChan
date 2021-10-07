19daf65e5e49ddcb1fd057fc23ab618c9fb808f7
jina <psalvarajah@lcs.on.ca>
2021-10-06 21:32:27 -0400

    Initial Commit

---
+++ AnimeChan/AnimeChanApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeChanApp.swift
+//  AnimeChan
+//
+//  Created by Salvarajah, Prajina on 2021-10-06.
+//
+
+import SwiftUI
+
+@main
+struct AnimeChanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
